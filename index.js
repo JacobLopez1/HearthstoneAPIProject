3114736da1475752c8b00b911f2e80232ce679a2
Jacob Lopez <lopezjacob2003.jl@gmail.com>
2022-07-08 16:49:43 -0600

    Search Bar Finish

--- index.js
+++ index.js
@@ -10,36 +10,36 @@ let activeSort = "MANA_LOW_TO_HIGH";
 let activeMana = "NONE";
 let activeSet = "voyage-to-the-sunken-city";
 let activeClass = "NONE";
+let lastId = "*";
+let activeSearch = "NONE";
 const cardsWrapper = document.querySelector(".card__search--list");
-console.log(cardsWrapper)
 
 async function getCards() {
   // Get All collectible cards in the Set
-  const cards = await fetch(
-    `https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/sets/%7B${activeSet}%7D`,
+  let cards = await fetch(
+    `https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/sets/%7B${activeSet}%7D/`,
     options
   );
+  if (activeSearch != "NONE") {
+    cards = await fetch(
+      `https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/${activeSearch}`,
+      options
+    ).catch(err => console.error(err), document.getElementById('search-bar').classList += ' not-active')
+  }
   const cardsData = await cards.json();
-  console.log(cardsData);
   cardsDataCollectible = cardsData.filter((card) => card.collectible);
-  console.log(cardsDataCollectible)
+  document.getElementById('search-bar').classList.remove('not-active')
 
   // Get Cards after Mana and Class Filters
-  if (activeMana === 'seven-up') {
-    cardsDataMana = cardsDataCollectible.filter(
-        (card) => card.cost >= 7
-    );
-  }
-  else if (activeMana != "NONE") {
+  if (activeMana === "seven-up") {
+    cardsDataMana = cardsDataCollectible.filter((card) => card.cost >= 7);
+  } else if (activeMana != "NONE") {
     cardsDataMana = cardsDataCollectible.filter(
       (card) => card.cost === activeMana
     );
-  } 
-  else {
+  } else {
     cardsDataMana = cardsDataCollectible;
   }
-
-  console.log(cardsDataMana)
 
   if (activeClass != "NONE") {
     cardsDataClass = cardsDataMana.filter(
@@ -52,55 +52,66 @@ async function getCards() {
   // Sort Cards by Mana Cost
   if (activeSort === "MANA_LOW_TO_HIGH") {
     cardsDataClass.sort((a, b) => a.cost - b.cost);
-  }
-  else if (activeSort === "MANA_HIGH_TO_LOW") {
-    cardsDataClass.sort((a,b) => b.cost - a.cost);
-  }
-  else if (activeSort === "NAME_A_TO_Z"){
-    cardsDataClass.sort((a, b) => a.name.localeCompare(b.name))
-  }
-  else {
-    cardsDataClass.sort((a, b) => b.name.localeCompare(a.name))
+  } else if (activeSort === "MANA_HIGH_TO_LOW") {
+    cardsDataClass.sort((a, b) => b.cost - a.cost);
+  } else if (activeSort === "NAME_A_TO_Z") {
+    cardsDataClass.sort((a, b) => a.name.localeCompare(b.name));
+  } else {
+    cardsDataClass.sort((a, b) => b.name.localeCompare(a.name));
   }
 
-  console.log(cardsDataClass);
   return cardsDataClass;
 }
 
 async function renderCards() {
-  const activeCards = await getCards()
-  console.log(activeCards)
-  const cardSetHTML = activeCards.map(card => cardHTML(card)).join("")
-  console.log(cardSetHTML)
+  const activeCards = await getCards();
+  const cardSetHTML = activeCards.map((card) => cardHTML(card)).join("");
   cardsWrapper.innerHTML = cardSetHTML;
 }
 
 renderCards();
 
-function cardHTML(card){
-    return `
+function cardHTML(card) {
+  return `
     <div class="card__wrapper">
         <img src="${card.img}" class="card__img">
         <p class="card__text">${card.flavor}</p>
-    </div>`
+    </div>`;
 }
 
 function classChange(event) {
-    activeClass = event.target.value
-    renderCards();
+  activeSearch = 'NONE'
+  activeClass = event.target.value;
+  renderCards();
 }
 
 function sortChange(event) {
-    activeSort = event.target.value
-    renderCards();
+  activeSearch = 'NONE'
+  activeSort = event.target.value;
+  renderCards();
 }
 
 function setChange(event) {
-    activeSet = event.target.value
-    renderCards();
+  activeSearch = 'NONE'
+  activeSet = event.target.value;
+  renderCards();
 }
 
 function manaChange(mana) {
-    activeMana = mana
+  activeSearch = 'NONE'
+  activeMana = mana;
+  renderCards();
+}
+
+function searchChange(event) {
+    activeSearch = event.target.value
     renderCards();
+}
+
+function toggleActive(id) {
+  if (lastId != "NONE") {
+    document.getElementById(lastId).classList += " not-active";
+  }
+  document.getElementById(id).classList.remove("not-active");
+  lastId = id;
 }
